b19b551f5998cdf1f62367a8fdadbb8de420fbe3
thomask <thomask@f9693dea-c825-0410-a691-c0b17bc6d315>
2004-10-27 17:26:54 +0000

    fixed package of GenReport

--- trunk/www/GenReport.java
+++ trunk/www/GenReport.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
  
-//package cn.kuehne.dmd.dstress;
+package cn.kuehne.dmd.dstress;
 
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
